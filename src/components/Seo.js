f360b510951c63a53fce2c83b7c47a35c292baf5
Tomas Losert <losert.tomas@gmail.com>
2022-09-23 08:26:54 +0200

    Revert "Refactor dependencies"
    This reverts commit 3193f3f376d7eca43115437aa8caab2a370e2abb.

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -10,8 +10,12 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 import { useIntl } from "gatsby-plugin-intl"
+import useLocalStorageState from "use-local-storage-state"
+import { v4 as uuidv4 } from "uuid"
 
 function Seo({ description, lang, meta, keywords, title }) {
+  const [gaId] = useLocalStorageState("gaId", uuidv4())
+
   const { site } = useStaticQuery(
     graphql`
       query {
